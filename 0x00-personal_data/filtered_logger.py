e309604a74cbe17a246127a8f6576b21d2de12fb
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-07 23:25:30 +0000

    Task 0 message_obfuscated.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,6 @@ import re
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ log message obfuscated """
     for i in fields:
-        message = re.sub(f'{i}=.*?(?={separator})',
-                         f'{i}={redaction}', message)
+        message = re.sub(f'{i}=.*?{separator}',
+                         f'{i}={redaction}{separator}', message)
     return message
