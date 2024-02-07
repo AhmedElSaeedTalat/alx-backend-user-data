125caf9168470bde0a7056ec2e890d9ac3464049
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-07 22:51:41 +0000

    Task 0 message_obfuscated...

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ log message obfuscated """
     pattern = '|'.join(list(map(lambda i: f'(?<={i}=).*?(?={separator})', fields)))
     return re.sub(pattern, redaction, message)
