9c1e1da62db683653406c5505b64c8945e55e40c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-08 00:27:09 +0000

    Task 0 message_obfuscated1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ module for message obfuscating """
 from typing import List
+import logging
 import re
 
 
@@ -8,3 +9,20 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """ filter log message to be obfuscated """
     pattern = '|'.join(list(map(lambda i: f'(?<={i}=).*?(?={separator})', fields)))
     return re.sub(pattern, redaction, message)
+
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(self.fields, self.redaction, record ,self.separator)
