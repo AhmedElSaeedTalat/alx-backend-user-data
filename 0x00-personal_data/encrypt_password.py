104fd884a71ada6035069d4a996b5c18917f5eb4
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-08 19:42:45 +0000

    Task 5 encrypt_password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ encrypt password """
 import bcrypt
+from typing import ByteString
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> ByteString:
     """ hash password function """
     password = password.encode('utf-8')
     salt = bcrypt.gensalt()
