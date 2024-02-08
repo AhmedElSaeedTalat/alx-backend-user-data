d46776849ce0da51092c2dcb8f4ac9081bf91579
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-08 15:46:00 +0000

    Task 2 usingGetLogger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -41,6 +41,6 @@ def get_logger() -> logging.Logger:
     logger = logging.getLogger(name='user_data')
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    handler.formatter(RedactingFormatter(PII_FIELDS))
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(handler)
     return logger
