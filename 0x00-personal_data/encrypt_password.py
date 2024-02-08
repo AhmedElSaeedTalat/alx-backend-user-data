27d86517decb6dd1db6262976cc3b47b3e05cbce
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-08 20:22:26 +0000

    Task 6 is_validBcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     password = password.encode()
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password, salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check if is valid """
+    password = password.encode()
+    return bcrypt.checkpw(password, hashed_password)
