5443d10d0f55d972d6011e6f046e05157201d0e4
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-08 16:59:25 +0000

    Task 3 connectDB

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -51,9 +51,9 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ return connector """
-    usr = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
-    ps = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    usr = os.getenv('PERSONAL_DATA_DB_USERNAME', "root")
+    ps = os.getenv('PERSONAL_DATA_DB_PASSWORD', "")
+    host = os.getenv('PERSONAL_DATA_DB_HOST', "localhost")
     db = os.getenv('PERSONAL_DATA_DB_NAME')
 
     """ connect to db """
