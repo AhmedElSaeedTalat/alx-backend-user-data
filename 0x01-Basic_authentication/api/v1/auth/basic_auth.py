3e06f3bbefa83bec1abe9ee21f89fd496c2a2842
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-13 18:49:29 +0000

    t8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,13 +2,14 @@
 """ BasicAuth to be used here """
 from .auth import Auth
 import re
+import base64
 
 
 class BasicAuth(Auth):
     """ BasicAuth class here"""
     def extract_base64_authorization_header(self, authorization_header: str) \
             -> str:
-        """ base64 encoding """
+        """ base64 extract pass """
         if not authorization_header:
             return None
         elif type(authorization_header) is not str:
@@ -18,3 +19,16 @@ class BasicAuth(Auth):
         pattern = r'(?<=Basic ).+'
         match = re.findall(pattern, authorization_header)
         return match[0]
+
+    def decode_base64_authorization_header(self,
+                                           base64_d: str) -> str:
+        """ decode base """
+        if not base64_d:
+            return None
+        elif type(base64_d) is not str:
+            return None
+        try:
+            decoded = base64.b64decode(base64_d)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
