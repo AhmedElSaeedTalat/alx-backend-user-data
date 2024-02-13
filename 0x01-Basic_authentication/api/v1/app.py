d07e194887da715f3c2bdfe66a9cd329dc4dfe76
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-13 17:25:25 +0000

    t6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
@@ -17,6 +16,10 @@ auth = None
 if getenv('AUTH_TYPE'):
     from api.v1.auth.auth import Auth
     auth = Auth()
+
+if getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
