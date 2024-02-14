b776115c17d37b278cf0f057867ce1f30c951dbd
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-14 22:57:49 +0000

    t2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """ BasicAuth to be used here """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ session Auth class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ function to create session """
+        if not user_id or type(user_id) is not str:
+            return None
+        id = uuid.uuid4()
+        self.user_id_by_session_id[id] = user_id
+        return id
