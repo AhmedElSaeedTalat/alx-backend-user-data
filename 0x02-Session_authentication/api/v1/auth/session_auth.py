f1899061184d99df5fac372a4f625b925ac28999
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-15 00:03:23 +0000

    t2 t3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,12 @@ class SessionAuth(Auth):
         """ function to create session """
         if not user_id or type(user_id) is not str:
             return None
-        id = uuid.uuid4()
+        id = str(uuid.uuid4())
         self.user_id_by_session_id[id] = user_id
         return id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ retrieve user id by session id """
+        if not session_id or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
