26c420dd245884bbc9b4c2de82a600da1261bcb1
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 17:54:42 +0000

    t6 create simple flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" flask app """
+from flask import Flask, jsonify
+app = Flask("__name__")
+
+
+@app.route('/', methods=['GET'])
+def home():
+    """ home function """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
