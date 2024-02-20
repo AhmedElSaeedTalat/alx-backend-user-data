fc78d873f3a00fb3ef5f2d4f1afcadc196feff34
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 19:05:59 +0000

    t8 find user and validate password passed

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,3 +29,19 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            validate ligin
+            Args:
+                email: email of the user
+                password: passord
+            Return - boolean
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
