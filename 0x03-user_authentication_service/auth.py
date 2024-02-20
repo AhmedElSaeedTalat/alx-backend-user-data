529dce4ac9d3f0077da85e18cab94f7865843e10
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 20:10:00 +0000

    t9 generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,6 +14,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password_encoded, salted)
 
 
+def _generate_uuid(self) -> str:
+    """ generates uuid """
+    return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -46,7 +51,3 @@ class Auth:
         except NoResultFound:
             return False
         return False
-
-    def _generate_uuid(self) -> str:
-        """ generates uuid """
-        return str(uuid4())
