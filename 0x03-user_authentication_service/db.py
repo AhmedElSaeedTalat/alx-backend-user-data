2a927822ec5cb836693be1767371c6d4f9f856ef
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 00:36:21 +0000

    t2 add filter user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -36,3 +37,20 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, *arg, **kwargs):
+        """ find user """
+        for key, val in kwargs.items():
+            try:
+                attribute = getattr(User, key)
+                user = self._session.query(User).filter(attribute == val)\
+                                                .first()
+            except AttributeError:
+                raise InvalidRequestError
+
+            if user:
+                break
+        if user:
+            return user
+        else:
+            raise NoResultFound
