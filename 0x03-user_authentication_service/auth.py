3de5d50b0c315a47fa68ff8f8c1fb7d9864d91c8
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 20:06:44 +0000

    t9 generate uuid

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -45,3 +46,7 @@ class Auth:
         except NoResultFound:
             return False
         return False
+
+    def _generate_uuid(self) -> str:
+        """ generates uuid """
+        return str(uuid4())
