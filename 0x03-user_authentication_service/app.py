48ca2769674716742202eb1c5db47a15b52888e8
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 18:31:16 +0000

    t7 register user through api

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ flask app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+AUTH = Auth()
 app = Flask("__name__")
 
 
@@ -8,6 +10,21 @@ app = Flask("__name__")
 def home():
     """ home function """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def add_users():
+    """ post method to register users """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        if user:
+            return jsonify({"email": "{}".format(email),
+                            "message": "user created"})
+
+    except ValueError:
+        return jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
