7965ad987acb287e2734b3af260fbd5b8a427019
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2024-02-20 21:46:18 +0000

    t11 create session method api

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ flask app """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 AUTH = Auth()
 app = Flask("__name__")
@@ -25,6 +25,19 @@ def add_users():
 
     except ValueError:
         return jsonify({"message": "email already registered"})
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def create_session():
+    """ creates session to user if exists"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": "<user email>", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    abort(401)
 
 
 if __name__ == "__main__":
